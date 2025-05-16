9b10bd865a1db384ac78d74bbe1d6f45f17dc423
Manas Gotefode <manasgotefode19@gmsil.com>
2025-05-16 22:37:00 +0530

    Initial commit

---
+++ src/main/java/com/research/assistant/ResearchRequest.java
@@ -0,0 +1,23 @@
+package com.research.assistant;
+
+public class ResearchRequest {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    private String operation;
+}
